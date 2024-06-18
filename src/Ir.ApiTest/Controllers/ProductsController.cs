2a71defd1a3bfe7f0d139876c711e09a3292c12b
JaredMackay <JaredMackay@paypoint.com>
2024-06-18 04:31:58 +0100

    Inject IProductService into the ProductsController and implement all the service logic in the controller methods

--- src/Ir.ApiTest/Controllers/ProductsController.cs
+++ src/Ir.ApiTest/Controllers/ProductsController.cs
@@ -1,34 +1,96 @@
+using Ir.ApiTest.Interfaces;
 using Ir.IntegrationTest.Contracts;
 using Microsoft.AspNetCore.JsonPatch;
 using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Collections.Generic;
+using System.Threading.Tasks;
 
-namespace Ir.FakeMarketplace.Controllers;
-
-[ApiController]
-[Route("api/[controller]")]
-public class ProductsController : ControllerBase
+namespace Ir.FakeMarketplace.Controllers
 {
-  [HttpGet]
-  public IEnumerable<Product> GetProducts()
+  [ApiController]
+  [Route("api/[controller]")]
+  public class ProductsController : ControllerBase
   {
-    throw new NotImplementedException();
-  }
+    private readonly IProductService _productService;
 
-  [HttpGet("{id}")]
-  public IActionResult GetProduct([FromRoute]string id)
-  {
-    throw new NotImplementedException();
-  }
+    public ProductsController(IProductService productService)
+    {
+      _productService = productService ?? throw new ArgumentNullException(nameof(productService));
+    }
 
-  [HttpPost()]
-  public IActionResult CreateProduct([FromBody] Product product)
-  {
-    throw new NotImplementedException();
-  }
+    [HttpGet]
+    public async Task<IActionResult> GetProducts()
+    {
+      try
+      {
+        var products = await _productService.GetProductsAsync();
+        return Ok(products);
+      }
+      catch(Exception ex)
+      {
+        return StatusCode(500, new { message = "An error occured while trying to fetch all the products", error = ex.Message });
+      }
+    }
 
-  [HttpPatch("{id}")]
-  public IActionResult UpdateProduct([FromBody] JsonPatchDocument<Product> productPatchDocument)
-  {
-    throw new NotImplementedException();
+    [HttpGet("{id}")]
+    public async Task<IActionResult> GetProduct([FromRoute] string id)
+    {
+      try
+      {
+        var product = await _productService.GetProductAsync(id);
+        if(product == null)
+        {
+          return NotFound(new { message = $"The following Product with ID: {id} not found" });
+        }
+        return Ok(product);
+      }
+      catch(Exception ex)
+      {
+        return StatusCode(500, new { message = $"An error occurred while fetching the product with ID: {id}", error = ex.Message });
+      }
+    }
+
+    [HttpPost]
+    public async Task<IActionResult> CreateProduct([FromBody] ProductDto product)
+    {
+      if(product == null)
+      {
+        return BadRequest(new { message = "Please input the required product data" });
+      }
+
+      try
+      {
+        var createdProduct = await _productService.CreateProductAsync(product);
+        return CreatedAtAction(nameof(GetProduct), new { id = createdProduct.Id }, createdProduct);
+      }
+      catch(Exception ex)
+      {
+        return StatusCode(500, new { message = "An error occurred while creating the product", error = ex.Message });
+      }
+    }
+
+    [HttpPatch("{id}")]
+    public async Task<IActionResult> UpdateProduct([FromRoute] string id, [FromBody] JsonPatchDocument<ProductDto> productUpdateParameters)
+    {
+      if(productUpdateParameters == null)
+      {
+        return BadRequest(new { message = "Error please input valid product update parameters in the request" });
+      }
+
+      try
+      {
+        var updatedProduct = await _productService.UpdateProductAsync(id, productUpdateParameters);
+        if(updatedProduct == null)
+        {
+          return NotFound(new { message = $"Product with ID: {id} could not be found" });
+        }
+        return Ok(updatedProduct);
+      }
+      catch(Exception ex)
+      {
+        return StatusCode(500, new { message = $"An error occurred while trying to update the product with ID: {id}", error = ex.Message });
+      }
+    }
   }
 }
